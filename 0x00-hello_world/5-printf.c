b50198fe571f870a8777ca95c7074801ed121e02
Heiswill <105241740+Heiswill@users.noreply.github.com>
2022-06-02 15:52:35 -0700

    printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+ /**
+  * main - prints another string
+  * 
+  * Return: 0
+  */
+void main()
+{
+      printf("with proper grammar, but the outcome is a piece of art, \n");
+      return (0);
+}
