122cf4a216071c014b28947f7936bf7fd63dfb38
Heiswill <willtriger@gmail.com>
2022-06-03 15:18:46 -0700

    single digits

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - print single digit of numbers of base 10(0 - 9)
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i;
+
+	for (i = 0; i < 10; i++)
+	{
+		printf("%d\n", i);
+	}
+	return (0);
+}
