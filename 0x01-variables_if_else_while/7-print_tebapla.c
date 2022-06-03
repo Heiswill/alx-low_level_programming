d51ea3cb56520233bc0b36bc1d5ee1d2711f82e9
Heiswill <willtriger@gmail.com>
2022-06-03 15:49:24 -0700

    tebahpla

---
+++ 0x01-variables_if_else_while/7-print_tebapla.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - prints alphabets in reverse order
+ *
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	char ch;
+
+	for (ch = 'z'; ch <= 'a'; ch--)
+	{
+		putchar(ch);
+		putchar('\n');
+	}
+	return (0);
+}
