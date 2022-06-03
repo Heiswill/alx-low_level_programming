bab837266d5ace50447d6e1c5a27b9f0bd347f54
Heiswill <willtriger@gmail.com>
2022-06-03 16:14:23 -0700

    combinations

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - print possible digits
+ *
+ * Return: 0
+ */
+int main(void)
+{
+	int j;
+
+	for (j = '0'; j <= '9'; j++)
+	{
+		if (j != '9')
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+	return (0);
+}
