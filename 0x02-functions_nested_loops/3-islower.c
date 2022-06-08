a9d4b4d6bbbf8e1293726cbb006816b9dd039501
Heiswill <willtriger@gmail.com>
2022-06-07 17:28:57 -0700

    functions

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -2,13 +2,15 @@
 
 /**
  * _islower - Checks if a character is lowercase.
- *
+ * @c: parameter argument
  * Return: 1 if lowercase, 0 otherwise.
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
+	{
 		return (1);
+	}
 	else
 		return (0);
 }
