72cbd519dac75d411846be448162aab9cc056f88
Heiswill <willtriger@gmail.com>
2022-06-08 19:08:56 -0700

    debug

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,15 @@
+#include "main.h"
+
+/**
+ * main - tests function that prints if a number is +ve or -ve.
+ * Return: 0
+ */
+int main(void)
+{
+	int i;
+
+	i = 0;
+	positive_or_negative();
+
+	return (0);
+}
