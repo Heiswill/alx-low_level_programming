f108a399e245046d43e6cdca8a50ecf8c8caf9fa
Heiswill <willtriger@gmail.com>
2022-06-09 12:22:52 -0700

    morenested

---
+++ 0x03-debugging/0x04-more_functions_nested_loops/main.h
@@ -0,0 +1,23 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdoi.h>
+
+/**
+ * File - main.h
+ * Desc: Header file containing declarations used in 
+ * 	 0x04-more_functions_nested_loops
+ */
+
+int _isupper(int c);
+int _isdigit(int c);
+int mul(int a, int b);
+void print_numbers(void);
+void print_most_numbers(void);
+void more_numbers(void);
+void print_line(int n);
+void print_diagonal(int n);
+void print_square(int size);
+void print_triangle(int size);
+
+#endif
