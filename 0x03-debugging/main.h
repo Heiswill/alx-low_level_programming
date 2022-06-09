16a00bfae6b3e88d2b89d60e672a244411e869e1
Heiswill <willtriger@gmail.com>
2022-06-08 20:33:52 -0700

    debug

---
+++ 0x03-debugging/main.h
@@ -0,0 +1,16 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+#include <stdio.h>
+
+/**
+ * File - main.h
+ * Desc: Header file containing declaration functiions used in
+ * 	 0x03-debugging directory.
+ */
+
+void positive_or_negative(int i);
+int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+
+#endif /* MAIN_H */
