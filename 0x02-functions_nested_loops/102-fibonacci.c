5692c810ea740013e4d6caa24471f8a3b24f70a0
Heiswill <willtriger@gmail.com>
2022-06-11 11:15:49 -0700

    fibonacci

--- 0x02-functions_nested_loops/102-fibonacci.c
+++ 0x02-functions_nested_loops/102-fibonacci.c
@@ -8,19 +8,22 @@
 
 int main(void)
 {
-	int a, b, result, n, i;
+        int i;
+        long int n[50];
 
-	a = 1;
-	b = 2;
-	n = 50;
+        n[0] = 1;
+        n[1] = 2;
+        printf("%ld, %ld, ", n[0], n[1]);
 
-	for (i = 1; i <= n; i++)
-	{
-		printf("%d, ", a);
-		result = a + b;
-		a = b;
-		b = result;
-	}
-	printf("\n");
-	return (0);
+        for (i = 1; i < 50; i++)
+        {
+                n[i] = n[i - 1] + n[i - 2];
+                if (i == 49)
+                {
+                        printf("%ld\n", n[i]);
+                }
+                else
+                        printf("%ld, ", n[i]);
+        }
+        return (0);
 }
