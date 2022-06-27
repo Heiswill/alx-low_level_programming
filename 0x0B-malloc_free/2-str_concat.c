3c4acb99370754fd6c425cea6cde9d6343370215
Heiswill <willtriger@gmail.com>
2022-06-27 11:46:37 -0700

    malloc

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -11,24 +11,36 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	int i = 0;
-	int s1_len = strlen(s1);
-	int s2_len = strlen(s2);
-	int tlen = s1_len + s2_len + 1;
+	int i = 0, lens1 = 0, lens2 = 0;
+	char *s, *str1, *str2;
 
-	char *s =malloc(sizeof(char) * tlen);
+	str1 = s1;
+	str2 = s2;
 
 	if (s1 == NULL)
 		s1 = "";
+	while (*s1)
+	{
+		lens1++;
+		s1++;
+	}
+	s1 = str1;
 	if (s2 == NULL)
 		s2 = "";
-
+	while (*s2)
+	{
+		lens2++;
+		s2++;
+	}
+	s2 = str2;
+	s = malloc(sizeof(char) * (lens1 + lens2 + 1));
+	str1 = s;
 	if (s == NULL)
 		return (NULL);
 
-	for (; i < tlen; i++)
+	for (; i < (lens1 + lens2); i++)
 	{
-		if (i < s1_len)
+		if (i < lens1)
 		{
 			s[i] = *s1;
 			s1++;
@@ -40,5 +52,5 @@ char *str_concat(char *s1, char *s2)
 		}
 	}
 	s[i] = '\0';
-	return (s);
+	return (str1);
 }
