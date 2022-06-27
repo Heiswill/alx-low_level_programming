c749368c9c409ccbfe7bb41ca029b72b4467fd29
Heiswill <willtriger@gmail.com>
2022-06-27 15:33:22 -0700

    malloc

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -28,6 +28,5 @@ int **alloc_grid(int width, int height)
 	for (i = 0; i < height; i++)
 		for (; j < width; j++)
 			arr[i][j] = 0;
-	return (arr);
-	free(arr);
+	return (arr);	
 }
---
+++ 0x0B-malloc_free/5-atgstostr.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * argstostr - function that concatenates all arguments
+ * @ac: number of arguments
+ * @av: argument array
+ * Return: pointer to the string containing all arguments,
+ * null upon failure
+ */
+char *argstostr(int ac, char **av)
+{
+	char *str;
+	int i = 0, len = 0, k = 0;
+
+	if (ac <= 0 || av == 0)
+		return (NULL);
+
+	for (; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+			len++;
+		len++;
+	}
+
+	len++;
+	str = malloc(len * sizeof(char));
+	if (str == NULL)
+		return (NULL);
+
+	for (i = 0; i < ac; i++)
+	{
+		for (j = 0; av[i][j]; j++)
+		{
+			str[k] = av[i][j];
+			k++;
+		}
+		str[k] = '\n';
+		k++;
+	}
+
+	str[k] = '\0';
+	return (str);
+}
