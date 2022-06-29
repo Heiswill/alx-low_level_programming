155e5236409f60ca47ef40237006dc3da48b88f5
Heiswill <willtriger@gmail.com>
2022-06-29 14:53:35 -0700

    Moremalloc

---
+++ 0x0C-more-malloc_free/101-mul.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - program to multiply two positive numbers.
+ * @argc: argument count
+ * @argv: argument array
+ * Return: 0
+ */
+int main(int argc, char **argv)
+{
+	int mul, num1, num2;
+
+	if (argc != 3)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+	else
+	{
+		num1 = atoi(argv[1]);
+		num2 = atoi(argv[2]);
+		mul = num1 * num2;
+		printf("%d\n", mul);
+		return (0);
+	}
+}
