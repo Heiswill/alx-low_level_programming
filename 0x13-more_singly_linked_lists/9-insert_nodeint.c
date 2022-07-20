9db7440251942776c542afb54f03a281d96e07d1
Heiswill <willtriger@gmail.com>
2022-07-20 14:20:48 -0700

    outputcheck

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -15,13 +15,25 @@ listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 	listint_t *ptr = *head;
 	listint_t *ptr2 = malloc(sizeof(listint_t));
 
+	if (ptr2 ==  NULL)
+		return (NULL);
 	ptr2->n = n;
 	ptr2->next = NULL;
-	idx--;
-	while (idx != 1)
+	if (idx == 0)
+	{
+		ptr2->next = ptr;
+		*head = ptr2;
+		return (*head);
+	}
+	while (idx > 1)
 	{
 		ptr = ptr->next;
 		idx--;
+		if (!ptr)
+		{
+			free(ptr2);
+			return (NULL);
+		}
 	}
 	ptr2->next = ptr->next;
 	ptr->next = ptr2;
