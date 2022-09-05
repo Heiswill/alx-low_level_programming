2a4b33496f7f4c0d284a7b32353febc9f8cc7d18
Heiswill <willtriger@gmail.com>
2022-09-05 14:14:03 -0700

    toxic

---
+++ 0x18-dynamic_libraries/9-strcpy.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _strcpy - a function that copies the string pointed to by src, including
+ * the terminating null byte(\0), to the buffer pointed to by dest
+ * @dest: pointer destination
+ * @src: pointer source to copy back
+ * Return: pointer to dest
+ */
+char *_strcpy(char *dest, char *src)
+{
+	char *temp = dest;
+
+	while (*src)
+		*dest++ = *src++;
+	return (temp);
+}
