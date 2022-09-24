3be7100f021ae2b1ee27b681b3b47a039ead4c01
Heiswill <willtriger@gmail.com>
2022-09-24 01:18:36 -0700

    fix1

--- 0x1A-hash_tables/0-hash_table_create.c
+++ 0x1A-hash_tables/0-hash_table_create.c
@@ -12,15 +12,13 @@ hash_table_t *hash_table_create(unsigned long int size)
 	if (size <= 0)
 		return (NULL);
 	new_table = malloc(sizeof(hash_table_t));
-	if (!new_table)
+	if (new_table == NULL)
 		return (NULL);
+
 	new_table->size = size;
-	new_table->array = malloc(size * sizeof(hash_node_t *));
-	if (!new_table->array)
-	{
-		free(new_table);
+	new_table->array = malloc(sizeof(hash_node_t *) * size);
+	if (new_table->array == NULL)
 		return (NULL);
-	}
 	for (i = 0; i < size; i++)
 		new_table->array[i] = NULL;
 	return (new_table);
