7673957d39cdb307c49e7baa090468ffbbed5bf9
Heiswill <willtriger@gmail.com>
2022-09-24 02:37:07 -0700

    fix

--- 0x1A-hash_tables/5-hash_table_print.c
+++ 0x1A-hash_tables/5-hash_table_print.c
@@ -1,36 +1,33 @@
 #include "hash_tables.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 /**
  *hash_table_print - prints a hash table in the order they appear
  *in the hash table array
  *@ht: pointer to the hash table
- *
- *Return: void
  */
 void hash_table_print(const hash_table_t *ht)
 {
-	unsigned long int iterator = 0;
+	unsigned long int cont = 0, i = 0;
 	hash_node_t *node;
-	int multiplenodes = 0;
 
-	if (ht == NULL)
-		return;
-
-	printf("{");
-	while (iterator < ht->size)
+	if (ht != NULL)
 	{
-		node = ht->array[iterator];
-		while (node != NULL)
+		printf("{");
+		for (cont = 0; cont < ht->size; cont++)
 		{
-			if (multiplenodes == 0)
-			{
-				printf("'%s': '%s'", node->key, node->value);
-				multiplenodes = 1;
-			}
-			else
-				printf(", '%s': '%s'", node->key, node->value);
-			node = node->next;
+			node = ht->array[cont];
+			while (node != NULL)
+				if (i != 0)
+				{
+					print(", ");
+					i = 1;
+					printf("'%s': '%s'", node->key, node->value);
+					node = node->next;
+				}
 		}
-		iterator++;
+		printf("}\n");
 	}
-	printf("}\n");
 }
