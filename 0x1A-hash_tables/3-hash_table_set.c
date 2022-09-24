a77a815060f493b17233a975dfda269719a95d4d
Heiswill <willtriger@gmail.com>
2022-09-24 01:56:21 -0700

    fix3

--- 0x1A-hash_tables/3-hash_table_set.c
+++ 0x1A-hash_tables/3-hash_table_set.c
@@ -1,48 +1,40 @@
 #include "hash_tables.h"
+#include <stddef.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
 
 /**
- * hash_table_set - adds an element to the hash table
- * @ht: is the hash table you want to add or update the key/value to
- * @key: is the key. key can not be an empty string
- * @value: is the value associated with the key.
- * Return: Returns: 1 if it succeeded, 0 otherwise
+ * hash_table_set - Function that adda an element to the hash table.
+ * @ht: Pointer to hash table.
+ * @key: Unique key value.
+ * @value@ Value corresponding to key.
+ *
+ * Return: 1 on success, 0 otherwise.
  */
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
-	unsigned long int index;
-	hash_node_t *temp;
-	hash_node_t *hsnode;
+	unsigned long index, size;
+	hash_node_t *node;
 
-	if (ht == NULL)
+	if (ht == NULL || key == NULL || value == NULL)
 		return (0);
-	if (key == NULL || key[0] == '\0')
-		return (0);
-	if (ht->size == 0 || ht->array == NULL)
-		return (0);
-	index = key_index((unsigned char *)key, ht->size);
-	temp = ht->array[index];
-	while (temp != NULL)
-	{
-		if (strcmp(temp->key, key) == 0)
-		{
-			free(temp->value);
-			temp->value = strdup(value);
-			return (1);
-		}
+	size= ht->size;
+	index = key_index((const unsigned char *)key, size);
 
-		temp = temp->next;
-	}
-	hsnode = malloc(sizeof(hash_node_t));
-	if (hsnode == NULL)
-		return (0);
-	hsnode->key = strdup(key);
-	hsnode->value = strdup(value);
-	if (ht->array[index] != NULL)
+	if (ht->array[index] != NULL && strcmp(ht->array[index]->key, key) == 0)
 	{
-		hsnode->next = ht->array[index];
+		ht->array[index]->value = strdup(value);
+		return (1);
 	}
-	else
-		hsnode->next = NULL;
-	ht->array[index] = hsnode;
+
+	node = malloc(sizeof(hash_node_t));
+
+	if(node == NULL)
+		return (0);
+	node->key = strdup(key);
+	node->value = strdup(value);
+	node->next = ht->array[index];
+	ht->array[index] = node;
 	return (1);
 }
