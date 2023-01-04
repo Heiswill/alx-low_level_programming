d14d04ead9124c2ea90707ac6430f74a020d0b4c
Heiswill <willtriger@gmail.com>
2023-01-04 09:20:21 -0800

    linear

---
+++ 0x1E-search_algorithms/0-linear.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "search_algos.h"
+
+/**
+ * linear_search - Function to linearly search xin arr[]. If x is
+ * present then return its location , otherwise return -1.
+ */
+int linear_search(int *array, size_t size, int value)
+{
+	int *arr = array;
+	size_t i;
+
+	for (i = 0; i < size; i++)
+	{
+		printf("Value checked array[%ld] = [%d]\n", i, arr[i]);
+		if (arr[i] == value)
+			return (i);
+	}
+	return (-1);
+}
