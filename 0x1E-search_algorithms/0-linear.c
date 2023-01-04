61fb2a4e0f9d7efe37747cc1663ad5a0c7e81dd1
Heiswill <willtriger@gmail.com>
2023-01-04 09:26:16 -0800

    linear

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -5,6 +5,10 @@
 /**
  * linear_search - Function to linearly search xin arr[]. If x is
  * present then return its location , otherwise return -1.
+ * @array: Array to traverse.
+ * @size: Size of array.
+ * @value: Data to find.
+ * Return: -1 if data not found.
  */
 int linear_search(int *array, size_t size, int value)
 {
